f942264e1516397fa313e3206e5552e59df4565d
Khoi <hakhoipham@gmail>
2018-08-22 08:47:52 -0400

    html skeleton done

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,1 +1,59 @@
-console.log('sup?');
+/* NAMESPACE */
+const app = {};
+
+/* 1. GET USER INPUT */
+// Purpose of travel
+app.getUserPurpose = () => {
+  // display stats question
+  // listen for the user click
+};
+
+/* 2. MAKE THE INQSTATS API REQUEST */
+app.getStatsAPIInfo = () => {
+  // based on click from user, send the predetermined statistic in the API request
+  // send ajax request to inqstats
+};
+
+/* 3. Calculate top 3 countries */
+app.calculateDestinations = () => {
+  //Filter through API response based on the top criteria
+};
+
+/* 4. Display destinations */
+app.displayDestinations = () => {
+  // Append top 3 countries to screen
+  // Explain briefly reasoning for the top destinations
+};
+
+/* 5. Reset if needed */
+app.reset = () => {
+  // reset app
+};
+
+/* ====================
+EXTRA STUFF
+====================== */
+
+/* 1. MAKE MAP */
+app.generateMap = () => {
+  // use JVQ library
+};
+
+// Selection and ranking of stats
+app.getUserRanking = () => {
+  // listen for user inputs
+};
+
+// Get info from Wikipedia (AJAX)
+app.getWiki = () => {
+  // get extract
+};
+
+// Get image
+app.getPixabay = () => {
+  // get country image(s)
+};
+
+// change color of three destination on map
+// append information cards to the countries in the map
+// append wiki + pixabay results
